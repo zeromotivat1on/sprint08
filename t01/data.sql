05a2ef27f3fb8930f72d14e73759cb3162354233
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-04-05 16:32:04 +0300

    t01

--- t01/data.sql
+++ t01/data.sql
@@ -1,31 +1,15 @@
 use ucode_web;
 
 INSERT INTO heroes (name, description, class_role)
-VALUES ('Noctis', 'Prince of Lucis', 'dps');
-
-INSERT INTO heroes (name, description, class_role)
-VALUES ('Gladiolus', 'Noctis\'s friend', 'tankman');
-
-INSERT INTO heroes (name, description, class_role)
-VALUES ('Ignis', 'Noctis\'s friend', 'dps');
-
-INSERT INTO heroes (name, description, class_role)
-VALUES ('Prompto', 'Noctis\'s friend', 'dps');
-
-INSERT INTO heroes (name, description, class_role)
-VALUES ('Iris', 'Noctis\'s friend and Gladiolus\'s sister', 'healer');
+VALUES ('Noctis', 'Prince of Lucis', 'dps'),
+('Gladiolus', 'Noctis\'s friend', 'tankman'),
+('Ignis', 'Noctis\'s friend', 'dps'),
+('Prompto', 'Noctis\'s friend', 'dps'),
+('Iris', 'Noctis\'s friend and Gladiolus\'s sister', 'healer');
 
 INSERT INTO heroes (name, description, race, class_role)
-VALUES ('Shiva', 'God of Ice', 'God', 'dps');
-
-INSERT INTO heroes (name, description, race, class_role)
-VALUES ('Arhei', 'God of Earth', 'God', 'dps');
-
-INSERT INTO heroes (name, description, race, class_role)
-VALUES ('Ramuh', 'God of Storm', 'God', 'dps');
-
-INSERT INTO heroes (name, description, race, class_role)
-VALUES ('Leviathan', 'God of Seas', 'God', 'dps');
-
-INSERT INTO heroes (name, description, race, class_role)
-VALUES ('Bahamut', 'God of War', 'God', 'dps');
+VALUES ('Shiva', 'God of Ice', 'God', 'dps'),
+('Arhei', 'God of Earth', 'God', 'dps'),
+('Ramuh', 'God of Storm', 'God', 'dps'),
+('Leviathan', 'God of Seas', 'God', 'dps'),
+('Bahamut', 'God of War', 'God', 'dps');
