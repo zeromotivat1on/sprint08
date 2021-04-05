d21c4855354d595f02a277b56517b99b3f266705
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-04-05 18:48:18 +0300

    t02

--- t02/extended.sql
+++ t02/extended.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS powers (
 CREATE TABLE IF NOT EXISTS races (
 
     id INT(10) AUTO_INCREMENT PRIMARY KEY,
-    hero_id INT(10) UNIQUE NOT NULL,
+    hero_id INT(10) NOT NULL,
     name TEXT NOT NULL,
 
     FOREIGN KEY(hero_id) REFERENCES heroes(id) ON DELETE CASCADE
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS races (
 CREATE TABLE IF NOT EXISTS teams (
 
     id INT(10) AUTO_INCREMENT PRIMARY KEY,
-    hero_id INT(10) UNIQUE NOT NULL,
+    hero_id INT(10) NOT NULL,
     name TEXT NOT NULL,
 
     FOREIGN KEY(hero_id) REFERENCES heroes(id) ON DELETE CASCADE
@@ -35,7 +35,11 @@ CREATE TABLE IF NOT EXISTS teams (
 
 INSERT INTO powers (hero_id, name, type, points) 
 VALUES (1, 'bloody fist', 'attack', 110),
-(1, 'iron shield', 'defense', 200);
+(1, 'iron shield', 'defense', 200),
+(2, 'bloody fist', 'attack', 150),
+(2, 'iron shield', 'defense', 90),
+(3, 'bloody fist', 'attack', 300),
+(4, 'iron shield', 'defense', 40);
 
 
 INSERT INTO races (hero_id, name)
@@ -45,4 +49,7 @@ VALUES (1, 'Human'),
 
 INSERT INTO teams (hero_id, name)
 VALUES (1, 'Avengers'),
-(2, 'Hydra');
+(1, 'Hydra'),
+(2, 'Hydra'),
+(4, 'Avengers'),
+(3, 'Avengers');
