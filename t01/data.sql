c41e82cbf98f132f9cb71513b0fc719edb7a695d
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-04-06 13:06:35 +0300

    t01

--- t01/data.sql
+++ t01/data.sql
@@ -12,4 +12,5 @@ VALUES ('Shiva', 'God of Ice', 'God', 'dps'),
 ('Arhei', 'God of Earth', 'God', 'dps'),
 ('Ramuh', 'God of Storm', 'God', 'dps'),
 ('Leviathan', 'God of Seas', 'God', 'dps'),
-('Bahamut', 'God of War', 'God', 'dps');
+('Bahamut', 'God of War', 'God', 'dps'),
+('lalka', 'unknwon', 'unknwon', 'healer');
