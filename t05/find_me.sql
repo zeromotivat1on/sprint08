57b76d934eb3e8fc6c202b907094faa563cb44f8
Alexander Lyannoy <alyannoy@c1r7p7.khpi.ucode-connect.study>
2021-04-06 13:06:57 +0300

    t05

--- t05/find_me.sql
+++ t05/find_me.sql
@@ -1,6 +1,6 @@
 USE ucode_web;
 
-SELECT res.name FROM (
+SELECT res.name, res.id FROM (
     SELECT heroes.id, heroes.name FROM heroes
-    WHERE heroes.name LIKE BINARY '%a%' AND heroes.race != 'human' AND heroes.class_role IN ('tankman','dps')
-) AS res JOIN teams ON res.id = teams.hero_id GROUP BY res.id HAVING COUNT(teams.hero_id) > 1 ORDER BY res.id ASC LIMIT 1;
+    WHERE heroes.name LIKE BINARY '%a%' AND heroes.race != 'human' AND heroes.class_role IN ('tankman','healer')
+) AS res JOIN teams ON res.id = teams.hero_id GROUP BY res.id /*HAVING COUNT(teams.hero_id) > 1*/ ORDER BY res.id ASC LIMIT 1;
